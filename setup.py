7fe9203b11bff8347ca3a25958f0e63705366c2e
David Chua <zhchua@gmail.com>
2016-04-10 11:02:28 +0800

    Bump to version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'nea_api',
   packages = ['nea_api'],
-  version = '0.1.0',
+  version = '0.1.1',
   install_requires=[
         'xmltodict','requests'
   ],
@@ -10,7 +10,7 @@ setup(
   author = 'David Chua',
   author_email = 'zhchua@gmail.com',
   url = 'https://github.com/davidchua/nea_api',
-  download_url = 'https://github.com/davidchua/nea_api/tarball/0.1.0',
+  download_url = 'https://github.com/davidchua/nea_api/tarball/0.1.1',
   keywords = ['data.gov.sg', 'python', 'wrapper', 'psi', 'singapore'],
   classifiers = [],
 )
